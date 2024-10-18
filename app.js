9d3c6de8e83b18895920748105df1caf499725d2
AsuSoftware <asu.software.dev@gmail.com>
2024-10-18 20:27:54 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware pentru parsarea JSON
+app.use(express.json());
+
+// Endpoint simplu GET
+app.get('/', (req, res) => {
+  res.send('API-ul funcționează!');
+});
+
+// Endpoint simplu POST
+app.post('/data', (req, res) => {
+  const data = req.body;
+  res.json({
+    message: 'Ai trimis următoarele date:',
+    data: data
+  });
+});
+
+// Pornirea serverului
+app.listen(port, () => {
+  console.log(`API-ul rulează la http://localhost:${port}`);
+});
